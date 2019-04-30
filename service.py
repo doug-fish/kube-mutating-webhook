80425a488b89e1060e984d763d9f25500c7d588e
Doug Fish <Fish.Douglas@mayo.edu>
2019-04-30 17:18:52 -0500

    initial /mutate implementation

--- service.py
+++ service.py
@@ -7,6 +7,22 @@ def service():
   print("entering /services")
   return "<div>Hi!</div>"
 
+@app.route('/mutate', methods=['POST'])
+def mutate(path):
+  print("entering /mutate")
+  content=response.get_json()
+  print("
+  data=content
+  response = app.response_class(
+    response=json.dumps(data),
+    status=200,
+    mimetype='application/json'
+  )
+  print("mutate response:")
+  print(response)
+  print("exiting /mutate")
+  return response
+
 @app.route('/<path:path>')
 def default(path):
   print("entering %s" % path)
