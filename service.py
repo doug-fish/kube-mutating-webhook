a9e5b409a49b0a78651f0d94fb820c0798bc9de5
Doug Fish <Fish.Douglas@mayo.edu>
2019-05-02 17:01:19 -0500

    some checking

--- service.py
+++ service.py
@@ -7,11 +7,20 @@ app = Flask(__name__)
 @app.route('/mutate', methods=['POST'])
 def mutate():
   print("entering /mutate")
-  content=request.get_json()
-  print(content)
-  data=content
+  requestData=request.get_json() or {}
+  print(requestData)
+  type=requestData.get('object', {}).get('spec', {}).get('type','')
+  annotations=requestData.get('object', {}).get('metadata', {}).get('annotations',{})
+  annotation=annotations.get('service.beta.kubernetes.io/azure-load-balancer-internal', 'false')
+  noAnnotation = not annotation.lower() == "true"
+
+  if type.lower == "loadbalancer" and noAnnotation:
+    print("will update")
+  else:
+    print("no update needed")
+
   response = app.response_class(
-    response=json.dumps(data),
+    response="hi",
     status=200,
     mimetype='application/json'
   )
