c3121717c60983847061284b678238916b7d8f29
Doug Fish <Fish.Douglas@mayo.edu>
2019-05-02 06:55:39 -0500

    add cluster cert script and improve output

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -1,2 +1,27 @@
-openssl req -x509 -newkey rsa:2048 -keyout tls.key -out tls.crt -nodes -subj '/CN=internal-service-webhook-svc.default.svc.cluster.local'
+openssl req -new -sha256 -key tls.key -out tls.csr -nodes -subj '/CN=internal-service-webhook-svc.default.svc.cluster.local'
+echo "generated CSR:"
+openssl req -noout -text -in tls.csr
+echo "submitting to kube"
+#kubectl create secret tls hook-tls --cert=tls.crt --key=tls.key
+
+cat <<EOF | kubectl apply -f -
+apiVersion: certificates.k8s.io/v1beta1
+kind: CertificateSigningRequest
+metadata:
+  name: internal-service-webhook-svc.default
+spec:
+  groups:
+  - system:authenticated
+  request: $(cat tls.csr | base64 | tr -d '\n')
+  usages:
+  - digital signature
+  - key encipherment
+  - server auth
+EOF
+
+kubectl certificate approve internal-service-webhook-svc.default
+
+kubectl get csr  internal-service-webhook-svc.default -o jsonpath='{.status.certificate}' \
+    | base64 --decode > tls.crt
+
 kubectl create secret tls hook-tls --cert=tls.crt --key=tls.key
--- service.sh
+++ service.sh
@@ -1,1 +1,2 @@
+echo "build date: $BUILD_DATE"
 python3.6 -m flask run
